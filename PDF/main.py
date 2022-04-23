205fc3b2da3b46291b52e9be84b7ceee1b3cdbb9
coulid <coulidyanmar@gmail.com>
2022-04-23 09:01:53 +0630

    PC

---
+++ PDF/main.py
@@ -0,0 +1,9 @@
+import PyPDF2
+
+import os
+
+file_names = os.popen("ls *An*.pdf").read();
+file_names = file_names.split("\n");
+
+
+for 
---
+++ PDF/run.sh
@@ -0,0 +1,6 @@
+while true
+do
+	clear
+	python3 main.py
+	sleep 3
+done
